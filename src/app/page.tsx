b7198a361f17db5ba161aaab13b654f51cb8a85e
viprobert <vip.robert14@gmail.com>
2026-01-15 16:32:47 +0700

    remove maintenance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,21 +6,10 @@ import Promotions from './components/Promotions';
 import Process from './components/process';
 import Faq from './components/faq';
 import GameTabs from './components/GameTabs';
-import MaintenancePopup from './components/MaintenancePopup';
-import { useState } from 'react';
 
 export default function F168Page() {
-  const [showPopup, setShowPopup] = useState<boolean>(true);
-  const closePopUp = () => {
-    setShowPopup(false);
-  };
-
   return (
     <div className={styles.container}>
-      <MaintenancePopup 
-        show={showPopup}
-        onClose={closePopUp}
-      />
       <Promo />
       <Promotions />
       <GameTabs />
